c0d6479423f8b0c737d3d3f4c193480c3b62285b
Crakchi <crakchi@gmail.com>
2025-06-06 21:36:11 -0500

    feat(): Ejercicio 2, OrdenarStack

--- src/main/java/AppEjercicios.java
+++ src/main/java/AppEjercicios.java
@@ -1,4 +1,6 @@
+import controllers.Stack;
 import ejercicio_01_sign.SignValidator;
+import ejercicio_02_sorting.StackSorter;
 
 public class AppEjercicios {
 
@@ -6,6 +8,47 @@ public class AppEjercicios {
         SignValidator signValidator = new SignValidator();
         System.out.println(signValidator.isValid("([]){}"));
         System.out.println(signValidator.isValid("({)}"));
+
+        Stack stack = new Stack();
+
+        stack.push(3);
+        stack.push(10);
+        stack.push(6);
+        stack.push(9);
+        stack.push(4);
+        stack.push(2);
+        stack.push(1);
+
+        stack.print();
+
+        StackSorter sorter = new StackSorter();
+        sorter.stackSort(stack);
+        System.out.println("\n");
+        stack.print();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
--- src/main/java/ejercicio_02_sorting/StackSorter.java
+++ src/main/java/ejercicio_02_sorting/StackSorter.java
@@ -1,4 +1,19 @@
 package ejercicio_02_sorting;
 
+import controllers.Stack;
+
 public class StackSorter {
+    public void stackSort(Stack temp) {
+        Stack stackTemp = new Stack();
+        while (!temp.isEmpty()) {
+            int elementoTemp = temp.pop();
+            while (!stackTemp.isEmpty() && stackTemp.peek() > elementoTemp) {
+                temp.push(stackTemp.pop());
+            }
+            stackTemp.push(elementoTemp);
+        }
+        while (!stackTemp.isEmpty()) {
+            temp.push(stackTemp.pop());
+        }
+    }
 }
